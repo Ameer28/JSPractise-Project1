7b82052404445e60d91b0c00ea167ff5c55bf0cf
Ameer Pestana <elearningviet88@gmail.com>
2020-09-05 23:37:16 +0800

    Project Practise
    Project Practise

--- app.js
+++ app.js
@@ -135,6 +135,24 @@ GAME RULES:
 
 */
 
+// var scores, roundScore, activePlayer, dice;
+
+// scores = [0,0];
+// roundScore = 0;
+// activePlayer = 0;
+
+// dice = Math.floor(Math.random() * 6) + 1;
+// console.log(dice);
+
+// //document.querySelector('#current-' + activePlayer).textContent = dice;
+
+// document.querySelector('#current-' + activePlayer).textContent = '<em>' + dice + '</em>';
+
+// var x = document.querySelector('#score-0').textContent;
+// console.log(x);
+
+// document.querySelector('.dice').style.display = 'none';
+
 var scores, roundScore, activePlayer, dice;
 
 scores = [0,0];
